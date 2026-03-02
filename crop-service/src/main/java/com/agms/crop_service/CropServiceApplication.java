8b7ee2a18380cb0408b040825d9f5281397d99a9
BhushithaHashan <bhushithahashan99@gmail.com>
2026-03-02 15:23:58 +0530

    aureka client and feign client enable for crop service

--- crop-service/src/main/java/com/agms/crop_service/CropServiceApplication.java
+++ crop-service/src/main/java/com/agms/crop_service/CropServiceApplication.java
@@ -2,12 +2,16 @@ package com.agms.crop_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient   // Tells Eureka "I am a service, please list me"
+@EnableFeignClients      // Tells Spring "Scan for @FeignClient interfaces and build them"
 public class CropServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CropServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CropServiceApplication.class, args);
+    }
 
 }

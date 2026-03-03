6351e0b82c5ba05e350a970d9c514f54b1363490
BhushithaHashan <bhushithahashan99@gmail.com>
2026-03-03 06:11:14 +0530

    sensor service init

--- sensor-service/src/main/java/com/agms/sensor_service/SensorServiceApplication.java
+++ sensor-service/src/main/java/com/agms/sensor_service/SensorServiceApplication.java
@@ -2,12 +2,14 @@ package com.agms.sensor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
 public class SensorServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SensorServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SensorServiceApplication.class, args);
+    }
 }
